a66cb83ea5db480166214d906382a8f0f415676a
Charlie Crosley <cc2190@york.ac.uk>
2021-11-19 23:57:06 +0000

    Updates

---
+++ precache-manifest.e86cbfbd8bc71de2a04f25b453651e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d245a45dc2967b7a957864080982e97b",
+    "url": "/CharlieCrosley/Pathfinder.git/index.html"
+  },
+  {
+    "revision": "42e9cefb0ad8b507892c",
+    "url": "/CharlieCrosley/Pathfinder.git/static/css/2.a31e845b.chunk.css"
+  },
+  {
+    "revision": "85e55be84e03f8f50022",
+    "url": "/CharlieCrosley/Pathfinder.git/static/css/main.c9a1e306.chunk.css"
+  },
+  {
+    "revision": "42e9cefb0ad8b507892c",
+    "url": "/CharlieCrosley/Pathfinder.git/static/js/2.82d0dffb.chunk.js"
+  },
+  {
+    "revision": "85e55be84e03f8f50022",
+    "url": "/CharlieCrosley/Pathfinder.git/static/js/main.7f10478c.chunk.js"
+  },
+  {
+    "revision": "221f298f4ec79d626ac3",
+    "url": "/CharlieCrosley/Pathfinder.git/static/js/runtime-main.15fe9f2e.js"
+  }
+]);
